3a9fd87711fdf2a912a54efe12b3a417c1518845
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-01 15:21:14 +0545

    Post Model Created

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=126)
+    body = models.TextField(max_length=1000)
+    published_on = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
