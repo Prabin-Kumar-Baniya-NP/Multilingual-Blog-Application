c9f8d34419a4e30ef74ca7da89ead8dd444aed87
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-01 15:59:16 +0545

    Comment app and model Created

---
+++ blog/comment/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from post.models import Post
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=124, null=True)
+    commented_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    upvote = models.IntegerField(default=0)
+    downvote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.body
