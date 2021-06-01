c6a476898d6c1ae5b7a30c44f2f9da83c97a3b69
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-01 15:43:42 +0545

    Category field added to post model

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
 class Post(models.Model):
@@ -6,6 +7,7 @@ class Post(models.Model):
     body = models.TextField(max_length=1000)
     published_on = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+    category = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return self.title
