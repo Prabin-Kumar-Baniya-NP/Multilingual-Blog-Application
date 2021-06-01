ef74d5bcb98e1f8552350dfc2948b202d92b482e
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-01 16:03:19 +0545

    Comment body changed from charfield to textfield

--- blog/comment/models.py
+++ blog/comment/models.py
@@ -3,7 +3,7 @@ from post.models import Post
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    body = models.CharField(max_length=124, null=True)
+    body = models.TextField(max_length=1000, null=True)
     commented_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     upvote = models.IntegerField(default=0)
