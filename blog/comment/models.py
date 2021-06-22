ef822c9acfe1013ee45ec6c6735c48e127bfc548
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-22 21:20:46 +0545

    Default value added

--- blog/comment/models.py
+++ blog/comment/models.py
@@ -3,11 +3,12 @@ from post.models import Post
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    body = models.TextField(max_length=1000, null=True)
+    body = models.TextField(max_length=1000)
     commented_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     upvote = models.IntegerField(default=0)
     downvote = models.IntegerField(default=0)
+    commented_by = models.CharField(max_length=48, default="None")
 
     def __str__(self):
         return self.body
