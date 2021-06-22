049dc721f645f8efc9a66e913644f8e6429894ff
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-22 21:19:54 +0545

    Default value added

--- blog/category/models.py
+++ blog/category/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=24)
-    description = models.TextField(max_length=126)
+    name = models.CharField(max_length=24, unique=True)
+    description = models.TextField(max_length=126, null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(max_length=48, default="None")
 
     def __str__(self):
         return self.name
