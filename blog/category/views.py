bfc0b3d8c2bb27ccd8fa957e2d76d7666ed1175e
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-22 21:21:25 +0545

    Category Creation and Manage view added

--- blog/category/views.py
+++ blog/category/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from category.forms import CategoryCreationForm
+from category.models import Category
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def create_category(request):
+    """
+    This view will to handle the creation of category objects
+    """
+    if request.method == "POST":
+        name = request.POST["name"]
+        description = request.POST["description"]
+        created_by = request.POST["created_by"]
+        new_category = Category.objects.create(name = name, description = description, created_by = created_by)
+        return HttpResponseRedirect(reverse("category:create-category"))
+    else:
+        context = {
+            'form': CategoryCreationForm(),
+        }
+        return render(request, "category/create-category.html", context)
+
+def manage_category(request):
+    """
+    This view will show all the categories which is created by the user
+    """
+    context = {
+        
+    }
+    return render(request, "category/manage-category-list.html", context)
