7b9ea8ff18dceffdc83b0162f03b5a5332659db3
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-22 21:21:54 +0545

    Default value added

--- blog/post/models.py
+++ blog/post/models.py
@@ -4,10 +4,11 @@ from category.models import Category
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=126)
-    body = models.TextField(max_length=1000)
+    body = models.TextField(max_length=1000, default="None")
     published_on = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     category = models.ManyToManyField(Category, blank=True)
+    author = models.CharField(max_length=48,default="None")
 
     def __str__(self):
         return self.title
