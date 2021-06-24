398e86df7b99d99ef59fbc898385b62823e97169
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-24 16:45:47 +0545

    Category Updation and deleting feature added

--- blog/category/urls.py
+++ blog/category/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import path
 from category import views
 
 app_name = "Category"
@@ -6,4 +7,6 @@ app_name = "Category"
 urlpatterns = [
     path("create-category/", views.create_category, name="create-category"),
     path("manage-category/", views.manage_category, name="manage-category"),
+    path("delete-category/<int:category_id>", views.delete_category, name="delete-category"),
+    path("update-category/<int:category_id>", views.update_category, name="update-category"),
 ]
--- blog/category/views.py
+++ blog/category/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from category.forms import CategoryCreationForm
+from category.forms import CategoryCreationForm, CategoryUpdationForm
 from category.models import Category
 from django.urls import reverse
 from django.http import HttpResponseRedirect
@@ -26,6 +26,32 @@ def manage_category(request):
     This view will show all the categories which is created by the user
     """
     context = {
-        
+        'categories': Category.objects.all()
     }
     return render(request, "category/manage-category-list.html", context)
+
+def delete_category(request, category_id):
+    """
+    This view will delete the requested category object
+    """
+    requested_category = Category.objects.get(id= category_id)
+    requested_category.delete()
+    return HttpResponseRedirect(reverse("category:manage-category"))
+
+def update_category(request, category_id):
+    """
+    This view will update the requested category
+    """
+    requested_category = Category.objects.get(id = category_id)
+    if (request.method == "POST"):
+        form = CategoryUpdationForm(request.POST, instance=requested_category)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("category:manage-category"))
+        else:
+            return HttpResponseRedirect(reverse("category:manage-category"))
+    else:
+        context = {
+            'form': CategoryUpdationForm(instance = requested_category),
+        }
+        return render(request, "category/update-category.html", context)
