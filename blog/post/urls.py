b5dfaad14a4493cdf89ebf82083885238f2982e0
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-26 14:24:06 +0545

    Post Urls for CRUD operations added

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -4,6 +4,9 @@ app_name = "post"
 
 urlpatterns = [
     path("create-post/", views.PostCreateView.as_view(), name="create-post"),
-    path("blog-dashboard/", views.PostListView.as_view(), name="blog-dashboard"),
-    path("view-post/<int:pk>", views.PostDetailView.as_view(), name="view-post"),
+    path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
+    path("view-post/<int:pk>/", views.PostDetailView.as_view(), name="view-post"),
+    path("manage-post/", views.ManagePostListView.as_view(), name="manage-post"),
+    path("update-post/<int:pk>/", views.PostUpdateView.as_view(), name="update-post"),
+    path("delete-post/<int:pk>/", views.PostDeleteView.as_view(), name="delete-post"),
 ]
