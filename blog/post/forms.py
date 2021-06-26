3bbc5b6825293c76ce997eac96c3a6f4e0c8057f
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-26 14:23:33 +0545

    Post Creation and Updation form added

--- blog/post/forms.py
+++ blog/post/forms.py
@@ -27,3 +27,29 @@ class PostCreationForm(forms.ModelForm):
                 'placeholder': "Enter your name",
             }),
         }
+
+class PostUpdationForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "body", "category", "author"]
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'id': "post-title",
+                'placeholder': "Enter the Post Title",
+            }),
+            'body': forms.Textarea(attrs={
+                'class': 'form-control',
+                'id': "post-body",
+                'placeholder': "Enter the Post Body",
+            }),
+            'category': forms.CheckboxSelectMultiple(attrs={
+                'class': "form-check-input",
+            }),
+            'author': forms.TextInput(attrs={
+                'class': 'form-control',
+                'id': "post-author",
+                'placeholder': "Enter your name",
+            }),
+        }
+
