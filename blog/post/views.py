e67b18888e2bb83619daef7f1873826a6417b1d3
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-26 14:24:41 +0545

    CRUD operations performed on post model

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView 
 from post.models import Post
-from post.forms import PostCreationForm
+from post.forms import PostCreationForm, PostUpdationForm
 from django.urls import reverse_lazy
 
 class PostCreateView(CreateView):
+    """
+    This view will handle creation of +
+    """
     model = Post
     form_class = PostCreationForm
     template_name = "post/create-post.html"
@@ -16,7 +19,7 @@ class PostCreateView(CreateView):
         form.save()
         return super().form_valid(form)
 
-class PostListView(ListView):
+class DashboardView(ListView):
     model = Post
     context_object_name = "posts"
     template_name = "post/blog.html"
@@ -27,3 +30,22 @@ class PostDetailView(DetailView):
     context_object_name = "post"
     template_name = "post/post.html"
 
+class ManagePostListView(ListView):
+    model = Post
+    context_object_name = "posts"
+    template_name = "post/manage-post-list.html"
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostUpdationForm
+    template_name = "post/update-post.html"
+    success_url = reverse_lazy("post:manage-post")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("post:manage-post")
+    
