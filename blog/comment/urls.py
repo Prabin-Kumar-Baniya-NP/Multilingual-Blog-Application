210664ee35bacd0a6ffbdc4efbf6da15413626ff
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-29 21:47:42 +0545

    path for ajax post request added

--- blog/comment/urls.py
+++ blog/comment/urls.py
@@ -3,5 +3,6 @@ from comment import views
 app_name = "Post"
 
 urlpatterns = [
-    path("get-comments/<int:postID>/<int:pnum>", views.get_comments_ajax, name="get-comments")    
+    path("get-comments/<int:postID>/<int:pnum>", views.get_comments_ajax, name="get-comments"), 
+    path("post-comment/", views.post_comment_ajax, name="post-comment")    
 ]
