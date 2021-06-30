602e3aa8c7d19ca7bf050fa61cc6b61bbd0d7dc2
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-30 19:46:01 +0545

    path added for comment delete view

--- blog/comment/urls.py
+++ blog/comment/urls.py
@@ -4,5 +4,6 @@ app_name = "Post"
 
 urlpatterns = [
     path("get-comments/<int:postID>/<int:pnum>", views.get_comments_ajax, name="get-comments"), 
-    path("post-comment/", views.post_comment_ajax, name="post-comment")    
+    path("post-comment/", views.post_comment_ajax, name="post-comment"),
+    path("delete-comment/<int:comment_id>/<int:post_id>", views.delete_comment, name = "delete-comment")    
 ]
