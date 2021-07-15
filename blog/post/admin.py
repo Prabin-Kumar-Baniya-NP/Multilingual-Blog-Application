0834c2ad202d892da6da98c5fc95004e349fe5e7
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-07-15 18:42:14 +0545

    Actions added in post and category modeladmin

--- blog/post/admin.py
+++ blog/post/admin.py
@@ -1,34 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import action
 from post.models import Post
 from django.contrib.auth.models import Group
 from django.contrib import messages
+
+@admin.action(description="Mark Selected as Approved")
+def approve(modeladmin, request, queryset):
+        queryset.update(status = "A")
+        messages.success(request, "Selected Item(s) Approved")
+
+@admin.action(description="Mark Selected as Rejected")
+def reject(modeladmin, request, queryset):
+    queryset.update(status = "R")
+    messages.success(request, "Selected Item(s) Rejected")
+
+@admin.action(description="Mark Selected as Blocked")
+def block(modeladmin, request, queryset):
+    queryset.update(status = "B")
+    messages.success(request, "Selected Item(s) Blocked")
+
+@admin.action(description="Mark Selected as Pending for Approval")
+def set_pending(modeladmin, request, queryset):
+    queryset.update(status = "P")
+    messages.success(request, "Selected Item(s) is set to Pending for Approval")
 
 class PostAdmin(admin.ModelAdmin):
     exclude = ("status",)
     list_display = ("title", "author", "status", "published_on")
     list_filter = ("status", "published_on")
     search_fields = ("title", "author__username")
+    actions = (approve, reject, block, set_pending)
 
-    def approve(modeladmin, request, queryset):
-        queryset.update(status = "A")
-        messages.success(request, "Selected Item(s) Approved")
-    
-    def reject(modeladmin, request, queryset):
-        queryset.update(status = "R")
-        messages.success(request, "Selected Item(s) Rejected")
-
-    def block(modeladmin, request, queryset):
-        queryset.update(status = "B")
-        messages.success(request, "Selected Item(s) Blocked")
-    
-    def set_pending(modeladmin, request, queryset):
-        queryset.update(status = "P")
-        messages.success(request, "Selected Item(s) is set to Pending for Approval")
-    
-    admin.site.add_action(approve, "Set as Approved")
-    admin.site.add_action(reject, "Set as Rejected")
-    admin.site.add_action(block, "Set as Blocked")
-    admin.site.add_action(set_pending, "Set as Pending for Approval")
 
 admin.site.register(Post, PostAdmin)
 admin.site.unregister(Group)
