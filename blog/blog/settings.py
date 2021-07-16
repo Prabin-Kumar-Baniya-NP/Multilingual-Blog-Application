157f6ce651fc5f60ac968671ef8c4917c6063f58
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-07-16 20:18:05 +0545

    Added test server

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cn_ndf_444^o#^s(y16#10izcg=rw(fsql5zc%fl#2htaavhhv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
 
 # Application definition
