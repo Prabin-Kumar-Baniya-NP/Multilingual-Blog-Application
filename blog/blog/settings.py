98c83e48c23f9beb4dd3c0fdc1f1250246336637
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-09-16 17:52:16 +0545

    debug set to True

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cn_ndf_444^o#^s(y16#10izcg=rw(fsql5zc%fl#2htaavhhv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 'localhost', 'testserver',
