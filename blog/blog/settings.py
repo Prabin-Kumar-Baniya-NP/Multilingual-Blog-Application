b97e523c3cf7789dc5db1953fcd0157a86b57d24
Prabin Kumar Baniya <pra44binind@gmail.com>
2021-09-17 16:39:35 +0545

    Updated for heroku db

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2'
+        }
     }
-}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
