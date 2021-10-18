47e4e245897e726e23c004fc13d40d92b4198043
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-10-18 19:16:14 +0545

    app name added

--- blog/category/urls.py
+++ blog/category/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from rest_framework import views
 from category import views
 
+
+app_name = "Category"
+
+
 urlpatterns = [
     path("", views.get_create_category, name="get-create-category"),
     path("<int:pk>/", views.update_delete_category, name="update-delete-category"),
