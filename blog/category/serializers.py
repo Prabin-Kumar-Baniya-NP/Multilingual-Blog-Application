e12b8e0166622906435963fe6ae00435459c7ddf
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-10-22 19:54:28 +0545

    Added Validation for created_on field

--- blog/category/serializers.py
+++ blog/category/serializers.py
@@ -2,11 +2,18 @@ from django.contrib.auth import models
 from django.db.models import fields
 from rest_framework.serializers import ModelSerializer
 from category.models import Category
+from rest_framework.exceptions import ValidationError
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
+        read_only_fields = ["created_on", "updated_on"]
+
+    def validate(self, attrs):
+        if self.context["request"].user.id != attrs["created_by"].id:
+            raise ValidationError(detail="created_by value must be equal to user id")
+        return super().validate(attrs)
 
 class CategorySerializerForPost(ModelSerializer):
     class Meta:
