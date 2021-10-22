b193195d6efb0ef58d5540aaea1f070d66e14d9a
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-10-22 19:55:39 +0545

    api view upaded for authenticated user

--- blog/category/views.py
+++ blog/category/views.py
@@ -4,7 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-
 @api_view(["GET", "POST"])
 def get_create_category(request):
     if request.method == "GET":
@@ -13,12 +12,15 @@ def get_create_category(request):
         return Response(serializer.data)
     
     elif request.method == "POST":
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if request.user.is_authenticated:
+            serializer = CategorySerializer(data=request.data, context={"request": request})
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(["GET","PUT", "DELETE"])
 def update_delete_category(request, pk):
@@ -32,20 +34,25 @@ def update_delete_category(request, pk):
         return Response(serializer.data)
     
     elif request.method == "PUT":
-        if category.created_by == request.user.id:
-            serializer = CategorySerializer(category, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
+        if request.user.is_authenticated:
+            if category.created_by.id == request.user.id:
+                serializer = CategorySerializer(category, data=request.data, context={"request": request})
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(status=status.HTTP_403_FORBIDDEN)
         else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         
     elif request.method == "DELETE":
-        if category.created_by == request.user.id:
-            category.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        if request.user.is_authenticated:
+            if category.created_by.id == request.user.id:
+                category.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response(status=status.HTTP_403_FORBIDDEN)
         else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
