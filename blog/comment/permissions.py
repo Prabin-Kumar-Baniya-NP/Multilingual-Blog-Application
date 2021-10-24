a19dcaece258f28d5fcb5df3d3d7b79747886c05
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-10-24 14:25:45 +0545

    comment crud api

---
+++ blog/comment/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+class CommentModifyPermission(BasePermission):
+    """
+    Permission for user to update/delete their comment
+    """
+    def has_object_permission(self, request, view, obj):
+        if obj.commented_by.id == request.user.id:
+            return True
+        else:
+            return False
